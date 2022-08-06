733b9a40f93c585b1702ae7fbbe86890bdaf041c
fanmiao11 <miao623@outlook.com>
2022-08-06 21:22:49 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: suiyue
+ * @email: 1373842098@qq.com
+ * @Date: 2022-07-30 20:09:42
+ * @LastEditors: sj
+ * @LastEditTime: 2022-07-30 20:19:25
+ */
+module.exports = {
+
+  title: 'likede',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
