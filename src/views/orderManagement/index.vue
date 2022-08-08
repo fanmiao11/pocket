463eb90ffee9c1e6442954fd042cc764800615fd
fanmiao11 <miao623@outlook.com>
2022-08-09 00:10:54 +0800

    静态页面搭建

--- src/views/orderManagement/index.vue
+++ src/views/orderManagement/index.vue
@@ -1,24 +1,55 @@
-<!--
- * @Descripttion:
- * @version:
- * @Author: suiyue
- * @email: 1373842098@qq.com
- * @Date: 2022-08-03 20:39:32
- * @LastEditors: sj
- * @LastEditTime: 2022-08-03 20:39:32
--->
 <template>
-  <div>
-    order management
+  <!-- 订单管理 -->
+  <div class="app-main">
+    <!-- 搜索 -->
+    <my-search nameOne="商品类型搜索" @search="searchBtn"> </my-search>
+    <!-- table -->
+    <result-list
+      :isShowBtn="false"
+      :isShowPagination="false"
+      :tableArr="tableArr"
+      :operation="operationArr"
+      :tableData="tableData"
+      @operationBtn="operationBtn"
+      class="result"
+    >
+    </result-list>
   </div>
 </template>
 
 <script>
+import MySearch from "@/components/Search.vue";
+import ResultList from "@/components/ResultList.vue";
 export default {
-
-}
+  data() {
+    return {
+      tableArr: [
+        { prop: "skuName", label: "订单编号" },
+        { prop: "skuImage", label: "商品名称" },
+        { prop: "brandName", label: "订单金额(元)" },
+        { prop: "unit", label: "设备编号" },
+        { prop: "price", label: "订单状态" },
+        { prop: "className", label: "订单日期" },
+      ],
+      operationArr: {
+        opeWidth: "100",
+        ope: [
+          // 操作
+          { title: "查看详情", color: false },
+        ],
+      },
+      tableData: [],
+    };
+  },
+  methods: {
+    searchBtn(val){},
+    operationBtn() {},
+  },
+  components: {
+    MySearch,
+    ResultList,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/tactical/index.vue
+++ src/views/tactical/index.vue
@@ -1,24 +1,53 @@
-<!--
- * @Descripttion:
- * @version:
- * @Author: suiyue
- * @email: 1373842098@qq.com
- * @Date: 2022-08-03 20:36:59
- * @LastEditors: sj
- * @LastEditTime: 2022-08-03 20:36:59
--->
 <template>
-  <div>
-    tactical
+  <!-- 策略管理 -->
+  <div class="app-main">
+    <!-- 搜索 -->
+    <my-search nameOne="策略搜索" @search="searchBtn"> </my-search>
+    <!-- table -->
+    <result-list
+      :isShowPagination="false"
+      :tableArr="tableArr"
+      :operation="operationArr"
+      :tableData="tableData"
+      @operationBtn="operationBtn"
+      class="result"
+    >
+    </result-list>
   </div>
 </template>
 
 <script>
+import MySearch from "@/components/Search.vue";
+import ResultList from "@/components/ResultList.vue";
 export default {
-
-}
+  data() {
+    return {
+      tableArr: [
+        { prop: "skuName", label: "策略名称" },
+        { prop: "skuImage", label: "策略方案" },
+        { prop: "brandName", label: "创建日期" },
+      ],
+      operationArr: {
+        opeWidth: "200",
+        ope: [
+          // 操作
+          { title: "查看详情", color: false },
+          { title: "修改", color: false },
+          { title: "删除", color: true },
+        ],
+      },
+      tableData: [],
+    };
+  },
+  methods: {
+    searchBtn(val){},
+    operationBtn() {},
+  },
+  components: {
+    MySearch,
+    ResultList,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
