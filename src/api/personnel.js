754f1726e3cb5bdcac039727f6b9ffe41a0c337f
huchun <1575724298@qq.com>
2022-08-09 14:05:45 +0800

    完成人效统计

--- src/api/personnel.js
+++ src/api/personnel.js
@@ -5,7 +5,7 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-08-07 10:40:03
  * @LastEditors: sj
- * @LastEditTime: 2022-08-08 18:11:09
+ * @LastEditTime: 2022-08-09 13:46:34
  */
 //  人员管理
 import request from '@/utils/request'
@@ -97,4 +97,27 @@ export const getUserWork = (params) => request({
 //获取用户基本信息
 export const getuserInfo = (id) => request({
   url: `/api/user-service/user/${id}`,
+})
+
+// 获取当时工单汇总信息(人员统计头部信息)
+export const getHeaderInfo = (start, end) => request({
+  url: `/api/task-service/task/taskReportInfo/${start}/${end}`,
+})
+
+// 人员排名
+/**
+ *
+ * @param {*} start
+ * @param {*} end
+ * @param {*} isRepair  true：运维工单;false：运营工单
+ * @param {*} regionId 地区ID
+ * @returns
+ */
+export const getToTen = (start, end, isRepair, regionId) => request({
+  url: `/api/task-service/task/userWorkTop10/${start}/${end}/${isRepair}/${regionId}`,
+})
+
+// 工单状态统计
+export const getCollectReport = (start, end) => request({
+  url: `/api/task-service/task/collectReport/${start}/${end}`,
 })
--- src/views/personnel/peopleCounting/index.vue
+++ src/views/personnel/peopleCounting/index.vue
@@ -5,7 +5,7 @@
  * @email: 1373842098@qq.com
  * @Date: 2022-08-03 20:21:46
  * @LastEditors: sj
- * @LastEditTime: 2022-08-09 01:12:55
+ * @LastEditTime: 2022-08-09 14:03:48
 -->
 <template>
   <div class="app-main">
@@ -14,19 +14,19 @@
         <div class="title">运营人员（当天）</div>
         <div class="body">
           <div>
-            <span>25</span>
+            <span>{{headerInfo[0].total}}</span>
             <span>工单总数（个）</span>
           </div>
           <div>
-            <span>25</span>
+            <span>{{headerInfo[0].completedTotal}}</span>
             <span>完成工单（个）</span>
           </div>
           <div>
-            <span>25</span>
+            <span>{{headerInfo[0].cancelTotal}}</span>
             <span>拒绝工单（个）</span>
           </div>
           <div>
-            <span>25</span>
+            <span>{{headerInfo[0].workerCount}}</span>
             <span>运营人员数（个）</span>
           </div>
         </div>
@@ -35,19 +35,19 @@
         <div class="title">运维人员（当天）</div>
         <div class="body">
           <div>
-            <span class="rightNumColor">25</span>
+            <span class="rightNumColor">{{headerInfo[1].total}}</span>
             <span class="rightDesColor">工单总数（个）</span>
           </div>
           <div>
-            <span class="rightNumColor">25</span>
+            <span class="rightNumColor">{{headerInfo[1].completedTotal}}</span>
             <span class="rightDesColor">完成工单（个）</span>
           </div>
           <div>
-            <span class="rightNumColor">25</span>
+            <span class="rightNumColor">{{headerInfo[1].cancelTotal}}</span>
             <span class="rightDesColor">拒绝工单（个）</span>
           </div>
           <div>
-            <span class="rightNumColor">25</span>
+            <span class="rightNumColor">{{headerInfo[1].workerCount}}</span>
             <span class="rightDesColor">运营人员数（个）</span>
           </div>
         </div>
@@ -56,7 +56,7 @@
     <el-row class="bottom" type="flex">
       <el-col class="box bom-left bom" style="margin-right: 25px">
         <div class="header">
-          <div class="title">工单状态</div>
+          <div class="title" style="margin-bottom:0">工单状态</div>
           <div class="date">
               <el-date-picker
                 class="time"
@@ -67,7 +67,7 @@
                 value-format="yyyy-MM-dd"
                 :start-placeholder="startTime"
                 :end-placeholder="endTime"
-                :disabledDate="nochoose"
+                :pickerOptions="pickerOptions"
               >
               </el-date-picker>
             <div class="week-month-year">
@@ -79,32 +79,91 @@
         </div>
 
         <div class="main">
-          <el-empty :image="emptyImg" description="暂无数据" class="empty"></el-empty>
+          <div  class="empty" v-if="isShowEmpty">
+            <img :src="emptyImg" alt="">
+            <p>暂无数据</p>
+          </div>
+
+          <div v-else>工单统计图</div>
         </div>
       </el-col>
-      <el-col class="box bom-right bom">4</el-col>
+      <el-col class="box bom-right bom">
+          <div class="header">
+          <div class="title" style="margin-bottom:0">人效排名（月度）</div>
+          <el-select placeholder="全部" class="select" v-model="area" @change="changeArea">
+             <el-option :label="item.name" :value="item.id" v-for="item in areaList" :key="item.id"></el-option>
+           </el-select>
+        </div>
+
+        <div class="role-group">
+           <div class="role-list">
+            <div class="role" :class="{isChecked: lCurrent}" @click="clickL">运营人员</div>
+            <div class="role" :class="{isChecked: rCurrent}" @click="clickR">运维人员</div>
+           </div>
+        </div>
+
+        <div class="role-content">
+           <div class="empty2" v-if="isShowEmpty2">
+            <img :src="emptyImg" alt="">
+            <p>暂无数据</p>
+          </div>
+          <div v-else> 排名表 </div>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import { getHeaderInfo,getAreaList,getToTen ,getCollectReport} from '@/api/personnel'
 import emptyImg from "@/assets/empty.87.png";
 import dayjs from "dayjs"
 export default {
   data() {
     return {
       value1:'',
-      startTime: '',
-      endTime: '',
+      startTime: dayjs(Date.now()).format("YYYY-MM-DD"),
+      endTime: dayjs(Date.now()).format("YYYY-MM-DD"),
       emptyImg,
       weekCurrent: true,
       monthCurrent: false,
       yearCurrent: false,
+      headerInfo: [{},{}],
+      pickerOptions:{
+       disabledDate(data){
+        return data > Date.now()
+       }
+      },
+      lCurrent: true, // 运营人员样式
+      rCurrent: false, // 运维人员样式
+      area:'',
+      areaList:[],
+      roleCurrent: false, //  当前选择的是哪个角色 true：运维工单;false：运营工单
+      isShowEmpty: false,
+      isShowEmpty2: false,
     }
   },
   created(){
-    this.endTime= this.startY = dayjs(Date.now()).format("YYYY-MM-DD")
    this.clickWeek()
+
+   this.getHeaderInfo(
+    dayjs(Date.now()).format("YYYY-MM-DD 00:00:00"),
+    dayjs(Date.now()).format("YYYY-MM-DD 23:59:59")
+   )
+
+   this.getAreaList({pageIndex:1,pageSize: 100000})
+
+   this.getToTen(
+     dayjs().startOf('month').format("YYYY-MM-DD"),
+     this.endTime,
+     this.roleCurrent,
+     0
+   )
+
+   this.getCollectReport(
+    dayjs().startOf('week').format(`YYYY-MM-DD`),
+    this.endTime
+   )
   },
   methods:{
     clickWeek(){
@@ -112,6 +171,7 @@ export default {
        this.monthCurrent=false
        this.yearCurrent=false
        this.startTime = dayjs().startOf('week').format(`YYYY-MM-DD`)
+       this.getCollectReport()
 
     },
     clickMonth(){
@@ -119,16 +179,68 @@ export default {
        this.monthCurrent=true
        this.yearCurrent=false
        this.startTime = dayjs().startOf('month').format("YYYY-MM-DD")
-
+       this.getCollectReport()
     },
     clickYear(){
        this.weekCurrent= false
        this.monthCurrent=false
        this.yearCurrent=true
        this.startTime = dayjs().startOf('year').format("YYYY-MM-DD")
+        this.getCollectReport()
     },
-    nochoose(data){
-      return true
+    //点击运营人员
+    clickL(){
+      this.lCurrent = true
+      this.rCurrent = false
+      this.roleCurrent = false
+      this.getAreaList(
+     dayjs().startOf('month').format("YYYY-MM-DD"),
+     this.endTime,
+     this.roleCurrent,
+     this.area
+      )
+    },
+    //点击运维人员
+     clickR(){
+      this.lCurrent = false
+      this.rCurrent = true
+      this.roleCurrent = true
+      this.getAreaList(
+     dayjs().startOf('month').format("YYYY-MM-DD"),
+     this.endTime,
+     this.roleCurrent,
+     this.area
+      )
+    },
+    // 头部列表数据  当天
+    async getHeaderInfo(start,end) {
+      const res = await getHeaderInfo(start,end)
+      if(res[0]) {this.headerInfo=res}
+    },
+    // 获取地区
+    async getAreaList(data){
+      const {currentPageRecords} = await getAreaList(data)
+      this.areaList=currentPageRecords
+    },
+    // 获取人员排名
+    async getToTen(start, end, isRepair, regionId){
+      const res = await getToTen(start, end, isRepair, regionId)
+      if(!res[0]){ this.isShowEmpty2 = true}
+    },
+    // 选择地区 重新获取排名
+    changeArea() {
+      this.getToTen(
+       dayjs().startOf('month').format("YYYY-MM-DD"),
+     this.endTime,
+     this.roleCurrent,
+     this.area
+      )
+    },
+    // 获取工单统计
+    async getCollectReport() {
+      const res = await getCollectReport( this.startTime,
+        this.endTime)
+       if(!res[0]){ this.isShowEmpty = true}
     }
   }
 };
@@ -141,7 +253,7 @@ export default {
 }
 
 .title {
-  font-size: 16px;
+  font-size: 18px;
   font-family: PingFangSC-Semibold, PingFang SC;
   font-weight: 600;
   color: #333;
@@ -222,6 +334,7 @@ export default {
   .header {
     display: flex;
     justify-content: space-between;
+    align-items: center;
   }
 
   .time{
@@ -275,11 +388,59 @@ export default {
     position: relative
   }
   .empty {
+    text-align: center;
     position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
+  .select {
+    width: 88px;
+
+   .el-input__inner {
+        height: 32px;
+        line-height: 32px;
+        padding-right: 30px;
+        cursor: pointer;
+   }
+  }
+
+  .role-group {
+    width: 177px;
+    margin: 15px auto 0;
+
+    .role-list {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 34px;
+    background: rgba(233,243,255,.37);
+    border-radius: 10px;
+
+    .role {
+     width: 78px;
+    height: 25px;
+    text-align: center;
+    line-height: 25px;
+    font-size: 14px;
+    color: #000;
+    cursor: pointer;
+    }
+    }
+  }
+
+    .role-content {
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
+
+   ::v-deep .empty2 {
+    text-align: center;
+    position: absolute;
+     top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+   }
 }
 </style>
