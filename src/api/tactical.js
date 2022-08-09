fc0aa0446bfb5a257ec5a362e0ffefb8974f175e
fanmiao11 <miao623@outlook.com>
2022-08-09 21:24:37 +0800

    完成策略管理

--- src/api/tactical.js
+++ src/api/tactical.js
@@ -8,10 +8,75 @@
  * @LastEditTime: 2022-08-09 14:21:07
  */
 //  策略管理
-import request from '@/utils/request'
+import request from "@/utils/request";
 
+export const getTacticalList = (params) =>
+  request({
+    url: "/api/vm-service/policy/search",
+    params,
+  });
 
-export const getTacticalList = (params) => request({
-  url: '/api/vm-service/policy/search',
-  params
-})
+  /**
+   * 新增策略
+   * @param {Object} data 策略名 折扣比例
+   * @returns 
+   */
+export function addpolicy(data){
+  return request({
+    url: "/api/vm-service/policy",
+    method:'POST',
+    data:{
+      policyName:data.policyName,
+      discount:Number(data.discount),
+    }
+  });
+}
+
+/**
+ * 修改策略
+ * @param {Object} data 策略id 策略名 折扣值
+ * @returns
+ */
+
+export function editpolicy(data) {
+  return request({
+    url: `/api/vm-service/policy/${data.policyId}`,
+    method: "PUT",
+    data: {
+      policyName: data.policyName,
+      discount: data.discount,
+    },
+  });
+}
+
+/**
+ * 删除策略
+ * @param {Object} data 策略id 策略名
+ * @returns 
+ */
+export function delpolicy(data){
+  return request({
+    url: `/api/vm-service/policy/${data.policyId}`,
+    method: "DELETE",
+    params:{
+      pageIndex:data.pageIndex,
+      pageSize:data.pageSize,
+      policyName:data.policyName
+    }
+  });
+}
+
+/**
+ * 根据策略id搜索售货机
+ * @param {Object} data 策略id 
+ * @returns 
+ */
+export function getvmlist(data){
+  return request({
+    url: `/api/vm-service/policy/vmList/${data.policyId}`,
+    params:{
+      pageIndex:data.pageIndex,
+      pageSize:data.pageSize
+    }
+  });
+}
--- src/views/tactical/index.vue
+++ src/views/tactical/index.vue
@@ -14,29 +14,116 @@
     <my-search nameOne="策略搜索" @search="searchBtn"> </my-search>
     <!-- table -->
     <result-list
-      :isShowPagination="false"
       :tableArr="tableArr"
       :operation="operationArr"
       :tableData="tableData"
+      :totalPage="totalPage"
+      :totalCount="totalCount"
+      :pageIndex="pageIndex"
+      @upPage="upPage"
+      @nextPage="nextPage"
       @operationBtn="operationBtn"
+      @clickAddBtn="clickAddBtn"
       class="result"
     >
     </result-list>
+    <!-- 新建 -->
+    <my-dialog
+      dialogTitle="新增商品类型"
+      :dialogVisible="opreationVisible"
+      @close="addOpreation = false"
+    >
+      <el-form ref="form" :model="formData" :rules="rules" label-width="120px">
+        <el-form-item label="商品类型名称：" prop="policyName">
+          <el-input
+            type="text"
+            v-model="formData.policyName"
+            placeholder="请输入"
+            maxlength="15"
+            show-word-limit
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="策略方案：" prop="discount">
+          <el-input-number
+            :style="{ width: '100%' }"
+            v-model="formData.discount"
+            controls-position="right"
+            :min="0"
+            :max="100"
+            :step="1"
+            placeholder="请输入"
+          ></el-input-number>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="cancleFn">取消</el-button>
+          <el-button @click="saveFn">确认</el-button>
+        </el-form-item>
+      </el-form>
+    </my-dialog>
+    <!-- 策略详情 -->
+    <el-dialog
+      title="策略详情"
+      :visible.sync="detailVisible"
+      width="50%"
+      :before-close="handleClose"
+    >
+      <el-form ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="策略名称:"
+          ><span>{{ currentRow.policyName }}</span></el-form-item
+        >
+
+        <el-form-item label="策略方案:">
+          <el-table
+            :data="details"
+            stripe
+            style="width: 100%"
+            empty-text="暂无数据"
+          >
+            <el-table-column prop="itemIndex" width="80" label="序号">
+            </el-table-column>
+            <el-table-column prop="nodeName" label="点位名称" width="180">
+            </el-table-column>
+            <el-table-column prop="innerCode" label="设备编号">
+            </el-table-column>
+          </el-table>
+        </el-form-item>
+        <el-form-item>
+          <my-pagination
+            :totalCount="vmtotalCount"
+            :totalPage="vmtotalPage"
+            :pageIndex="vmpageIndex"
+            @upPage="upPage"
+            @nextPage="nextPage"
+          ></my-pagination>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getTacticalList } from '@/api/tactical'
+import {
+  getTacticalList,
+  addpolicy,
+  editpolicy,
+  delpolicy,
+  getvmlist,
+} from "@/api/tactical";
 import MySearch from "@/components/Search.vue";
 import ResultList from "@/components/ResultList.vue";
+import MyDialog from "@/components/Dialog.vue";
+import MyPagination from "@/components/Pagination.vue";
+
 export default {
-  created(){},
   data() {
+    const validatetype = async (rule, value, callback) => {
+      callback();
+    };
     return {
       tableArr: [
-        { prop: "skuName", label: "策略名称" },
-        { prop: "skuImage", label: "策略方案" },
-        { prop: "brandName", label: "创建日期" },
+        { prop: "policyName", label: "策略名称" },
+        { prop: "discount", label: "策略方案" },
+        { prop: "createTime", label: "创建日期" },
       ],
       operationArr: {
         opeWidth: "200",
@@ -48,19 +135,211 @@ export default {
         ],
       },
       tableData: [],
+      pageIndex: "",
+      totalPage: "",
+      totalCount: "",
+      addOpreation: false,
+      editOpreation: false,
+      detailVisible: false,
+      formData: {
+        policyName: "",
+        discount: undefined,
+        policyId: "",
+      },
+      rules: {
+        policyName: [
+          { required: true, message: "请输入", trigger: "blur" },
+          { validator: validatetype, trigger: "blur" },
+        ],
+        discount: [
+          { required: true, message: "请输入", trigger: "blur" },
+          { validator: validatetype, trigger: "blur" },
+        ],
+      },
+      currentRow: {},
+      details: [],
+      vmpageIndex: "",
+      vmtotalPage: "",
+      vmtotalCount: "",
     };
   },
+  created() {
+    this.getTacticalList();
+  },
+  computed: {
+    opreationVisible() {
+      return this.addOpreation || this.editOpreation;
+    },
+  },
   methods: {
-    searchBtn(val){},
-    operationBtn() {},
-    async getTacticalList(){
-      const res = await getTacticalList()
-      console.log(res);
-    }
+    // 搜索按钮
+    searchBtn(val) {
+      this.formData.policyName = val;
+      this.getTacticalList(this.formData);
+    },
+    // 点击新建
+    clickAddBtn() {
+      this.addOpreation = true;
+      // console.log(this.addOpreation);
+    },
+
+    // 取消按钮
+    cancleFn() {
+      if (this.addOpreation) {
+        this.addOpreation = false;
+      } else if (this.editOpreation) {
+        this.editOpreation = false;
+      }
+      // 重置校验规则和表单数据
+      this.$refs.form.resetFields();
+      this.formData = {
+        policyName: "",
+        discount: undefined,
+        policyId: "",
+      };
+    },
+    // 确定按钮
+    saveFn() {
+      // console.log(this.formData);
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          if (this.addOpreation) {
+            // 添加策略
+            const res = await addpolicy(this.formData);
+            // console.log(res);
+            this.getTacticalList();
+            this.addOpreation = false;
+            // console.log("添加");
+          } else if (this.editOpreation) {
+            // 修改策略
+            await editpolicy(this.formData);
+            this.getTacticalList();
+            this.editOpreation = false;
+            // console.log("修改");
+          }
+          this.formData = {
+            policyName: "",
+            discount: undefined,
+            policyId: "",
+          };
+        } else {
+          console.log("操作失败");
+          return false;
+        }
+      });
+    },
+    handleClose(done) {
+      done();
+    },
+    // 操作
+    async operationBtn(row, val) {
+      if (val === "查看详情") {
+        console.log("查看详情");
+        this.detailVisible = true;
+        this.currentRow = row;
+        // console.log(this.currentRow);
+        const res = await getvmlist({
+          policyId: row.policyId,
+        });
+        // console.log(res);
+        this.details = res.currentPageRecords;
+        this.vmpageIndex = res.pageIndex;
+        this.vmtotalPage = res.totalPage;
+        this.vmtotalCount = res.totalCount;
+        this.details.forEach((item, index) => {
+          item.itemIndex = (this.vmpageIndex - 1) * 10 + index + 1;
+        });
+      } else if (val === "修改") {
+        console.log("修改");
+        this.editOpreation = true;
+        this.formData.policyName = row.policyName;
+        this.formData.discount = row.discount;
+        this.formData.policyId = row.policyId;
+      } else if (val === "删除") {
+        console.log("删除");
+        this.$confirm("此操作将永久删除该策略, 是否继续?", "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+          center: true,
+        })
+          .then(async () => {
+            await delpolicy({
+              policyName: row.policyName,
+              policyId: row.policyId,
+            });
+            this.$message({
+              type: "success",
+              message: "删除成功!",
+            });
+            this.getTacticalList();
+          })
+          .catch(() => {
+            this.$message({
+              type: "info",
+              message: "已取消删除",
+            });
+          });
+      }
+    },
+    // 获取数据
+    async getTacticalList(params) {
+      const res = await getTacticalList(params);
+      // console.log(res);
+      this.tableData = res.currentPageRecords;
+      this.pageIndex = res.pageIndex;
+      this.totalPage = res.totalPage;
+      this.totalCount = res.totalCount;
+      this.tableData.forEach((item, index) => {
+        item.itemIndex = (this.pageIndex - 1) * 10 + index + 1;
+      });
+    },
+    // 点击上一页
+    async upPage() {
+      if (this.detailVisible) {
+        const res = await getvmlist({
+          policyId: this.currentRow.policyId,
+          pageIndex: --this.vmpageIndex,
+        });
+        this.details = res.currentPageRecords;
+        this.vmpageIndex = res.pageIndex;
+        this.details.forEach((item, index) => {
+          item.itemIndex = (this.vmpageIndex - 1) * 10 + index + 1;
+        });
+      } else {
+        this.getskuClass({
+          policyName: this.formData.policyName,
+          pageIndex: --this.pageIndex,
+          pageSize: 10,
+        });
+      }
+    },
+    // 点击下一页
+    async nextPage() {
+      if (this.detailVisible) {
+        const res = await getvmlist({
+          policyId: this.currentRow.policyId,
+          pageIndex: ++this.vmpageIndex,
+        });
+        this.details = res.currentPageRecords;
+        this.vmpageIndex = res.pageIndex;
+        this.details.forEach((item, index) => {
+          item.itemIndex = (this.vmpageIndex - 1) * 10 + index + 1;
+        });
+      } else {
+        this.getTacticalList({
+          policyName: this.formData.policyName,
+          pageIndex: ++this.pageIndex,
+          pageSize: 10,
+        });
+      }
+    },
   },
   components: {
     MySearch,
     ResultList,
+    MyDialog,
+    MyPagination,
   },
 };
 </script>
